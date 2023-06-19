5a95abfcb5ee13c33ead21bebe38ec1c6a7cb290
REbrecht <tigger1005@users.noreply.github.com>
2023-06-19 11:32:12 +0000

    Initial commit

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <math.h>
+#include "functions.h"
 
 
 unsigned int main(void)
 {
     printf("Hello World!\n");
+    printf("%f\n", M_PI);
+    double test = sin(0.46);
+    printf("Sin: %lf\n, %d",test, add(10,2));
+    
     return 0;
 }
+
